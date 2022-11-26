023481590de77200ed62d1005a52a20cb9d66473
Viljami Mäkelä <viljami.j.m@gmail.com>
2022-11-26 23:40:57 +0200

    TEMPORARY: Change default theme to dark mode

--- app/src/main/java/com/example/reseptit/StartActivity.kt
+++ app/src/main/java/com/example/reseptit/StartActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 
 class StartActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) // TODO: Default dark mode?
 
         val timer = object: CountDownTimer(3000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
