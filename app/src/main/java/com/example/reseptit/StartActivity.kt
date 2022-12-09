577b816191c9861a18e72e48eac9c8ac61a6c00b
Viljami Mäkelä <viljami.j.m@gmail.com>
2022-12-09 14:25:21 +0200

    Revert "TEMPORARY: Change default theme to dark mode"
    This reverts commit 023481590de77200ed62d1005a52a20cb9d66473.

--- app/src/main/java/com/example/reseptit/StartActivity.kt
+++ app/src/main/java/com/example/reseptit/StartActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 
 
 class StartActivity : AppCompatActivity() {
@@ -13,7 +12,6 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) // TODO: Default dark mode?
 
         val timer = object: CountDownTimer(3000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
